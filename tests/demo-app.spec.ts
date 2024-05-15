c1141f5c30dbeb4228b35ee549cacca49c8a0cb6
ketakipillay <ketakipillay@gmail.com>
2024-05-15 14:24:45 -0400

    Update demo-app.spec.ts

--- tests/demo-app.spec.ts
+++ tests/demo-app.spec.ts
@@ -15,7 +15,7 @@ test.describe('New Todo', () => {
   test('should allow me to add todo items', async ({ page }) => {
     // create a new todo locator
     const newTodo = page.getByPlaceholder('What needs to be done?');
-
+    console.log("envVariables.BASEURL :"+envVariables.BASEURL);
     // Create 1st todo.
     await newTodo.fill(TODO_ITEMS[0]);
     await newTodo.press('Enter');
