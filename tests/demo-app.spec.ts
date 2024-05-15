0c14cc65d4f998ea5acf6c451ec50f2a8e1b219f
ketakipillay <ketakipillay@gmail.com>
2024-05-15 14:17:17 -0400

    Using env variable

--- tests/demo-app.spec.ts
+++ tests/demo-app.spec.ts
@@ -2,7 +2,7 @@ import { test, expect, type Page } from '@playwright/test';
 import { envVariables } from '../utils/envVariables'; 
 
 test.beforeEach(async ({ page }) => {
-  await page.goto("https://demo.playwright.dev/todomvc/#/");
+  await page.goto(envVariables.BASEURL);
 });
 
 const TODO_ITEMS = [
